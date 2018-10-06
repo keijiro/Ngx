2bd2acafb5486f787812935eae9973660bae6375
Keijiro Takahashi <keijiro@gmail.com>
2018-10-06 22:27:48 +0900

    Weight reader optimization

--- Assets/Pix2Pix/WeightReader.cs
+++ Assets/Pix2Pix/WeightReader.cs
@@ -61,8 +61,13 @@ namespace Pix2Pix
                 var info = shapes[i];
                 length = info.shape.Aggregate(1, (acc, x) => acc * x);
 
+                UnityEngine.Profiling.Profiler.BeginSample("Weight data decoding");
+
+                var bytes = reader.ReadBytes(length);
                 var data = new float[length];
-                for (var j = 0; j < length; j++) data[j] = values[reader.ReadByte()];
+                for (var j = 0; j < length; j++) data[j] = values[bytes[j]];
+
+                UnityEngine.Profiling.Profiler.EndSample();
 
                 var tensor = new Tensor(new Shape(info.shape), data);
 
