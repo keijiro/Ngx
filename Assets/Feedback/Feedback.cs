630461de57719d49e9093d75ac1092381c8f1c85
Keijiro Takahashi <keijiro@gmail.com>
2018-10-07 23:24:02 +0900

    simplified feedback.cs

--- Assets/Feedback/Feedback.cs
+++ Assets/Feedback/Feedback.cs
@@ -7,17 +7,30 @@ namespace Pix2Pix
     {
         #region Editable attributes
 
-        [SerializeField] string [] _weightFileNames = null;
+        [SerializeField, Range(0, 2)] float _feedbackRate = 1;
+        [SerializeField, Range(0.01f, 2)] float _transitionTime = 0.5f;
+        [SerializeField, Range(0, 16)] int _modelIndex = 0;
+        [SerializeField] string [] _modelFiles = null;
         [SerializeField, HideInInspector] Shader _shader = null;
 
         #endregion
 
         #region Internal objects
 
-        Dictionary<string, Pix2Pix.Tensor> [] _weights;
+        Dictionary<string, Pix2Pix.Tensor> [] _models;
         Pix2Pix.Generator _generator;
-        RenderTexture _feedbackRT;
+
+        RenderTexture _tempRT, _delayRT, _backRT;
         Material _material;
+
+        int _prevModelIndex;
+        float _transition;
+
+        string GetModelFilePath(int index)
+        {
+            var file = _modelFiles[index] + ".pict";
+            return System.IO.Path.Combine(Application.streamingAssetsPath, file);
+        }
 
         #endregion
 
@@ -25,50 +38,84 @@ namespace Pix2Pix
 
         void Start()
         {
-            _weights = new Dictionary<string, Pix2Pix.Tensor>[_weightFileNames.Length];
+            _models = new Dictionary<string, Pix2Pix.Tensor>[_modelFiles.Length];
 
-            for (var i = 0; i < _weightFileNames.Length; i++)
-                _weights[i] = Pix2Pix.WeightReader.ReadFromFile(
-                    System.IO.Path.Combine(
-                        Application.streamingAssetsPath, _weightFileNames[i]));
+            for (var i = 0; i < _modelFiles.Length; i++)
+                _models[i] = Pix2Pix.WeightReader.ReadFromFile(GetModelFilePath(i));
 
             _generator = new Pix2Pix.Generator();
 
-            _feedbackRT = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat);
-            _feedbackRT.enableRandomWrite = true;
-            _feedbackRT.Create();
+            _tempRT  = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat);
+            _delayRT = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat);
+            _backRT  = new RenderTexture(256, 256, 0, RenderTextureFormat.ARGBFloat);
+
+            _tempRT.enableRandomWrite = true;
+
+            _tempRT .Create();
+            _delayRT.Create();
+            _backRT .Create();
 
             _material = new Material(_shader);
+
+            _prevModelIndex = _modelIndex;
+            _transition = 1e+5f;
         }
 
         void OnDestroy()
         {
-            foreach (var w in _weights) Pix2Pix.WeightReader.DisposeTable(w);
+            foreach (var m in _models) Pix2Pix.WeightReader.DisposeTable(m);
+
             _generator.Dispose();
-            Destroy(_feedbackRT);
+
+            Destroy(_tempRT);
+            Destroy(_delayRT);
+            Destroy(_backRT);
+
             Destroy(_material);
         }
 
         void Update()
         {
-            var select = (Time.frameCount / 30) % _weights.Length;
-            _generator.WeightTable = _weights[select];
+            // Set the currently selected model to the generator.
+            _generator.WeightTable = _models[_modelIndex % _models.Length];
 
-            _generator.Start(_feedbackRT);
+            // Pix2Pix generation
+            _generator.Start(_delayRT);
             while (true)
             {
                 _generator.Step();
                 if (!_generator.Running) break;
             }
-            _generator.GetResult(_feedbackRT);
-
+            _generator.GetResult(_tempRT);
             Pix2Pix.GpuBackend.ExecuteAndClearCommandBuffer();
+
+            // Was the model changed?
+            if (_modelIndex != _prevModelIndex)
+            {
+                _transition = 0; // Start fade-in
+                _prevModelIndex = _modelIndex;
+            }
+
+            // Transition animation
+            _transition += Time.deltaTime;
+
+            var alpha = Mathf.Clamp01(_transition / _transitionTime);
+            var blendParams = new Vector2(1 - alpha, alpha) * _feedbackRate;
+
+            // Blend with the previous frame.
+            _material.SetTexture("_BlendTex", _tempRT);
+            _material.SetVector("_BlendParams", blendParams);
+            Graphics.Blit(_delayRT, _backRT, _material, 1);
+
+            var rt = _backRT;
+            _backRT = _delayRT;
+            _delayRT = rt;
         }
 
         void OnRenderObject()
         {
             if ((Camera.current.cullingMask & (1 << gameObject.layer)) == 0) return;
-            _material.SetTexture("_MainTex", _feedbackRT);
+            _material.SetTexture("_MainTex", _delayRT);
             _material.SetPass(0);
             Graphics.DrawProcedural(MeshTopology.Triangles, 3, 1);
         }
--- Assets/Feedback/Feedback.shader
+++ Assets/Feedback/Feedback.shader
@@ -3,6 +3,7 @@ Shader "Hidden/Pix2Pix/Feedback"
     Properties
     {
         _MainTex("", 2D) = "" {}
+        _BlendTex("", 2D) = "" {}
     }
 
     CGINCLUDE
@@ -10,8 +11,10 @@ Shader "Hidden/Pix2Pix/Feedback"
     #include "UnityCG.cginc"
 
     sampler2D _MainTex;
+    sampler2D _BlendTex;
+    half2 _BlendParams;
 
-    void Vertex(
+    void VertexBlit(
         uint vid : SV_VertexID,
         out float4 position : SV_Position,
         out float2 texcoord : TEXCOORD
@@ -24,12 +27,19 @@ Shader "Hidden/Pix2Pix/Feedback"
         texcoord = float2(x * 2 - 0, 2 - y * 2);
     }
 
-    half4 Fragment(
+    half4 FragmentBlit(
         float4 position : SV_Position,
         float2 texcoord : TEXCOORD
     ) : SV_Target
     {
         return tex2D(_MainTex, texcoord);
+    }
+
+    half4 FragmentBlend(v2f_img i) : SV_Target
+    {
+        half4 c1 = tex2D(_MainTex, i.uv) * _BlendParams.x;
+        half4 c2 = tex2D(_BlendTex, i.uv) * _BlendParams.y;
+        return saturate(c1 + c2);
     }
 
     ENDCG
@@ -40,8 +50,15 @@ Shader "Hidden/Pix2Pix/Feedback"
         Pass
         {
             CGPROGRAM
-            #pragma vertex Vertex
-            #pragma fragment Fragment
+            #pragma vertex VertexBlit
+            #pragma fragment FragmentBlit
+            ENDCG
+        }
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert_img
+            #pragma fragment FragmentBlend
             ENDCG
         }
     }
